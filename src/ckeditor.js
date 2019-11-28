dbac4e43c151cf89508255910985fb91e2edb176
Thierry Belliot <thierry@watermelon.co>
2019-11-28 09:26:38 +0100

    custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,94 +7,81 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Emoji from '@wwalc/ckeditor5-emoji/src/emoji';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import InsertImage from './insertImage';
+
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
+	Paragraph,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
+	Underline,
+	FontFamily,
+	FontSize,
 	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar
+	Image,
+	InsertImage,
+	Emoji
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
-			'|',
 			'bold',
 			'italic',
+			'underline',
+			'|',
+			'FontFamily',
+			'FontSize',
+			'|',
 			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'indent',
-			'outdent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
+			'insertImage',
+			'emoji'
 		]
 	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
+	emoji: [
+		{ name: 'smile', text: '🙂' },
+		{ name: 'sad', text: '🙁' },
+		{ name: 'wink', text: '😉' },
+		{ name: 'happy', text: '😄' },
+		{ name: 'confusion', text: '😕' },
+		{ name: 'tongue', text: '😜' },
+		{ name: 'flushed', text: '😳' },
+		{ name: 'neutral', text: '😐' },
+		{ name: 'angry', text: '😠' },
+		{ name: 'pouting', text: '😡' },
+		{ name: 'holy', text: '😇' },
+		{ name: 'nerd', text: '🤓' },
+		{ name: 'evil', text: '😈' },
+		{ name: 'cry', text: '😭' },
+		{ name: 'idea', text: '💡' },
+		{ name: 'down', text: '👎' },
+		{ name: 'up', text: '👍' },
+		{ name: 'heart', text: '❤️' },
+		{ name: 'broken', text: '💔' },
+		{ name: 'kiss', text: '😘' },
+		{ name: 'letter', text: '📧' },
+	],
+	fontSize: {
+		options: [8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72]
 	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
+	uiColor: '#F2F2F2',
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
